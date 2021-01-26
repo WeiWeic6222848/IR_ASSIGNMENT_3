27bf51c9aed7ce871d8e799c3c74a43c6283aa9e
Wei Wei <Wei.Wei@student.uantwerpen.be>
2021-01-26 13:50:06 +0100

    preprocessing and tokenizing

--- src/Util.py
+++ src/Util.py
@@ -3,19 +3,41 @@ class NewsArticle():
         self.ID = ID
         self.content = content
 
+        content = content.casefold()
+        token = dict.fromkeys(content.split())
+        for word in stopwords:
+            token.pop(word, None)  # remove all frequent stopwords
+
+        self.token = list(token)
+
 
 def load_dataset_to_csv(filename):
     with open(filename) as f:
         f.readline()  # skip line 1
         parsing = f.readline()
         result = []
-        while len(parsing)>0:
-            parsing = parsing.split(",",maxsplit=1)
-            parsing[1]=parsing[1].strip().strip("\"")
+        while len(parsing) > 0:
+            parsing = parsing.split(",", maxsplit=1)
+            parsing[1] = parsing[1].strip().strip("\"")
             result.append(NewsArticle(parsing[0], parsing[1]))
             parsing = f.readline()
         return result
 
 
+stopwords = {"ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out",
+             "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into",
+             "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the",
+             "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were",
+             "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to",
+             "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have",
+             "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can",
+             "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself",
+             "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by",
+             "doing", "it", "how", "further", "was", "here", "than"}
 
-print(len(load_dataset_to_csv("dataset/news_articles_small.csv")))
+
+
+
+loaded=load_dataset_to_csv("dataset/news_articles_small.csv")
+article = loaded[0]
+print(article.token)
