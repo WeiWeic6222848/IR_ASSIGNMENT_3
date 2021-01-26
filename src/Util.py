c2c32428cd1bf0768109d68d4b657b534f445266
PraveshGurung <s0174500@ad.ua.ac.be>
2021-01-26 14:27:38 +0100

    shingles,hashing and minhash

--- src/Util.py
+++ src/Util.py
@@ -1,8 +1,10 @@
 class NewsArticle():
-    def __init__(self, ID, content):
-        self.ID = ID
-        self.content = content
-
+    def __init__(self,ID, content, tokens):
+        self.ID= ID
+        self.content= content
+        self.tokens = tokens
+        self.shingles = []
+        self.min_hashed_shingles = []
 
 def load_dataset_to_csv(filename):
     with open(filename) as f:
@@ -16,6 +18,41 @@ def load_dataset_to_csv(filename):
             parsing = f.readline()
         return result
 
+def make_shingles(articles):
+    for article in articles:
+        for i in range(len(article.tokens)-1):
+            article.shingles.append(article.tokens[i]+" "+article.tokens[i+1])
+
+def hash_shingles(articles):
+    for article in articles:
+        for i in range(len(article.shingles)):
+            article.shingles[i] = hash(article.shingles[i])
+
+def hash_k(k,number):
+    return number % (1000000*k)
+
+def minhash_shingles(articles,k): #k = number of hash functions to use
+    for article in articles:
+        for j in range(k):
+            for i in range(len(article.shingles)):
+                article.shingles[i] = hash_k(j+1,article.shingles[i])
+            article.min_hashed_shingles.append(min(article.shingles))
 
 
-print(len(load_dataset_to_csv("dataset/news_articles_small.csv")))
+
+#test
+na = NewsArticle(1,"The quick brown fox",["The","quick","brown","fox"])
+na2 = NewsArticle(2,"The lazy blue fox",["The", "lazy", "blue", "fox"])
+result=[na,na2]
+make_shingles(result)
+hash_shingles(result)
+minhash_shingles(result,8)
+
+print(na.shingles[0])
+
+#print(len(load_dataset_to_csv("dataset/news_articles_small.csv")))
+#result = load_dataset_to_csv("dataset/news_articles_small.csv")
+
+
+
+
