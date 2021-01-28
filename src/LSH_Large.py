444f31b6f7fe45e6b64dd6772cb65538aa4cd0da
Wei Wei <Wei.Wei@student.uantwerpen.be>
2021-01-28 14:51:21 +0100

    LSH_large and result

--- src/LSH_Large.py
+++ src/LSH_Large.py
@@ -17,19 +17,26 @@ if __name__ == "__main__":
     hash_shingles(data)
     minhash_shingles(data, hash_funcs(40))  # M=40
     buckets = LSH(data, 0.3, 0.8, preferRecall=True)  # s1=0.3,s2=0.8, maximize recall -> b=10, r=4
-    result = []
+    result = set()
+    totalfound = 0
+    found = set()
     for candidate in buckets.values():
         # iterate through each of the buckets
         if len(candidate) > 1:
             # if there are more than one element
             for i1 in range(len(candidate)):
                 article1 = data[int(candidate[i1])]  # assume ID = row ID
-                for i2 in range(i1+1, len(candidate)):
+                for i2 in range(i1 + 1, len(candidate)):
                     article2 = data[int(candidate[i2])]  # assume ID = row ID
-
+                    if ((article1.ID, article2.ID) not in found and (article2.ID, article1.ID) not in found):
+                        totalfound += 1
+                        found.add((article1.ID, article2.ID))
                     # calculate Jaccard index
                     score = Jaccard_sim(article1.shingles, article2.shingles)
                     # if Jaccard index is bigger than 0.8, add to result
+
                     if score >= 0.8:
-                        result.append((article1.ID, article2.ID))
+                        result.add((article1.ID, article2.ID))
+
+    print("found:{},correct:{}".format(totalfound, result))
     write_result_to_csv(result)
